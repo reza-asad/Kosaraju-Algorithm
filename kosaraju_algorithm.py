cfa7ad1c8ba9185f092e00d776ad66820fcbc964
reza-asad <rayza.asad@gmail.com>
2016-01-24 15:15:49 -0800

    the basic structure of the algorithm

--- kosaraju_algorithm.py
+++ kosaraju_algorithm.py
@@ -4,11 +4,31 @@
 ######################### Algorithms ######################
 # Implementation of the strongly conncected components of 
 # a directed graph
+explored = set()
+finishing_time = {}
+leader = {}
 def SCC(G):
-	pass
+	n = len(G.keys())
+	for i in range(n, -1, -1):
+		if i not in explored:
+			dfs(G, i)
+	def dfs(G, i):
+		pass
 ######################## Main ###############################
+# Creating the reversed graph fot the first pass of the algorithm
+from collections import defaultdict
 
+graph = defaultdict(set)
 data_file = open("SCC.txt", "rb")
 for line in data_file:
-	print int(line.split()[0]), int(line.split()[1])
-	break 
+	graph[int(line.split()[1])].add(int(line.split()[0]))
+
+SCC(graph, 1)
+
+# Creating the graph fot the second pass of the algorithm
+graph = defaultdict(set)
+data_file = open("SCC.txt", "rb")
+for line in data_file:
+	graph[int(line.split()[0])].add(int(line.split()[1]))
+
+SCC(graph, 0)
