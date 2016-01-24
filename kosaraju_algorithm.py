ea582ab0f2256b61d6c61002caaab98d9775a9b3
reza-asad <rayza.asad@gmail.com>
2016-01-24 15:30:09 -0800

    dfs but different book keeping for original and reversed graph

--- kosaraju_algorithm.py
+++ kosaraju_algorithm.py
@@ -7,16 +7,29 @@
 explored = set()
 finishing_time = {}
 leader = {}
-def SCC(G, forward):
+def SCC(G, reverse):
+	t = 0
 	n = len(G.keys())
 	for i in range(n, -1, -1):
 		if i not in explored:
+			if reverse == 0:
+				s = i
 			dfs(G, i)
 	def dfs(G, i):
-		if forward == 1:
-			pass
-		else:
-			pass
+		explored.add(i)
+		if reverse == 0:
+			leader[i] = s
+		for j in G[i]:
+			if j not in  explored:
+				dfs(G, j)
+		
+		if reverse == 1:
+			t += 1
+			finishing_time[i] = t
+
+
+
+
 ######################## Main ###############################
 # Creating the reversed graph fot the first pass of the algorithm
 from collections import defaultdict
@@ -26,7 +39,7 @@ data_file = open("SCC.txt", "rb")
 for line in data_file:
 	graph[int(line.split()[1])].add(int(line.split()[0]))
 
-SCC(graph, 0)
+SCC(graph, 1)
 
 # Creating the graph fot the second pass of the algorithm
 graph = defaultdict(set)
@@ -34,4 +47,7 @@ data_file = open("SCC.txt", "rb")
 for line in data_file:
 	graph[int(line.split()[0])].add(int(line.split()[1]))
 
-SCC(graph, 1)
+SCC(graph, 0)
+
+
+
