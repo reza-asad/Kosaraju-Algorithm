dfaf2a1c3a0d58bb6ef62c53c6600aeae91e739a
reza-asad <rayza.asad@gmail.com>
2016-01-24 14:24:45 -0800

    reading the data

---
+++ kosaraju_algorithm.py
@@ -0,0 +1,14 @@
+# Reza Asad
+# Algorithm Class
+# Jan 24th, 2016
+######################### Algorithms ######################
+# Implementation of the strongly conncected components of 
+# a directed graph
+def SCC(G):
+	pass
+######################## Main ###############################
+
+data_file = open("SCC.txt", "rb")
+for line in data_file:
+	print int(line.split()[0]), int(line.split()[1])
+	break 
