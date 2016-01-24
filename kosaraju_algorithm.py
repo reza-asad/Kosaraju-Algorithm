1fa7418c5d386f0793c953f94a7ffb3d44d531f9
reza-asad <rayza.asad@gmail.com>
2016-01-24 15:17:36 -0800

    the basic structure of the algorithm

--- kosaraju_algorithm.py
+++ kosaraju_algorithm.py
@@ -7,13 +7,16 @@
 explored = set()
 finishing_time = {}
 leader = {}
-def SCC(G):
+def SCC(G, forward):
 	n = len(G.keys())
 	for i in range(n, -1, -1):
 		if i not in explored:
 			dfs(G, i)
 	def dfs(G, i):
-		pass
+		if forward == 1:
+			pass
+		else:
+			pass
 ######################## Main ###############################
 # Creating the reversed graph fot the first pass of the algorithm
 from collections import defaultdict
@@ -23,7 +26,7 @@ data_file = open("SCC.txt", "rb")
 for line in data_file:
 	graph[int(line.split()[1])].add(int(line.split()[0]))
 
-SCC(graph, 1)
+SCC(graph, 0)
 
 # Creating the graph fot the second pass of the algorithm
 graph = defaultdict(set)
@@ -31,4 +34,4 @@ data_file = open("SCC.txt", "rb")
 for line in data_file:
 	graph[int(line.split()[0])].add(int(line.split()[1]))
 
-SCC(graph, 0)
+SCC(graph, 1)
