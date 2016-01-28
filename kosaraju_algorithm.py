d80eee95045dfe7341d5e815add0a1bcc9615db6
reza-asad <rayza.asad@gmail.com>
2016-01-27 21:28:06 -0800

    seperating the reverse and teh forward dfs and fixing bugs

--- kosaraju_algorithm.py
+++ kosaraju_algorithm.py
@@ -10,11 +10,8 @@
 #		graph is the original graph or if it is reversed
 # output:
 #	leader: The strongly connected componets of the graph
-import sys
-sys.setrecursionlimit(15000)
 
 finishing_time = {}
-leader = {}
 
 def SCC(G, is_reverse):
 	def dfs(i):
@@ -29,50 +26,52 @@ def SCC(G, is_reverse):
 				leader[v] = s[0]
 				stack.extend(G[v] - explored)
 
-		for i in range(len(seen) - 1, -1, -1):
-			t[0] += 1
-			finishing_time[t[0]] = seen[i]
+		if is_reverse == 1:
+			for i in range(len(seen) - 1, -1, -1):
+				t[0] += 1
+				finishing_time[t[0]] = seen[i]
 
 	explored = set()
+	leader = {}
 	n = len(G)
 	t = [0]
 	s = [0]
 	i = n
 	while i > 0:
-		if i not in explored:
-			if is_reverse == 0:
+		if is_reverse == 1:
+			if i not in explored:
+				dfs(i)
+		elif is_reverse == 0:
+			if finishing_time[i] not in explored:
 				s[0] = i
 				dfs(finishing_time[i])
-			else:
-				dfs(i)
+				
 		i -= 1
-
+	#print 'finishing time: ' + str(finishing_time)
+	#print 'leaders: ' + str(leader)
+	print sorted(Counter(leader.values()).values())[-20:]
 
 ######################## Main ###############################
 # Creating the reversed graph fot the first pass of the algorithm
-#from collections import defaultdict
+from collections import defaultdict, Counter
 
-#graph = defaultdict(set)
-#data_file = open("SCC.txt", "rb")
-#for line in data_file:
-#	graph[int(line.split()[1])].add(int(line.split()[0]))
+graph = defaultdict(set)
+data_file = open("SCC.txt", "rb")
+for line in data_file: 
+	graph[int(line.split()[1])].add(int(line.split()[0]))
+	print int(line.split()[1]), int(line.split()[0]) 
+
+#graph = {1:{3}, 3:{2, 4}, 2:{1}, 4:{6}, 6:{5}, 5:{4}}
 
 #SCC(graph, 1)
 
 # Creating the graph fot the second pass of the algorithm
-#graph = defaultdict(set)
-#data_file = open("SCC.txt", "rb")
-#for line in data_file:
-#	graph[int(line.split()[0])].add(int(line.split()[1]))
+graph = defaultdict(set)
+data_file = open("SCC.txt", "rb")
+for line in data_file:
+	graph[int(line.split()[0])].add(int(line.split()[1]))
 
+#graph = {1:{2}, 2:{3}, 3:{1}, 4:{3,5}, 5:{6}, 6:{4}}
 #SCC(graph, 0)
 
 
-graph = {1:{7}, 4:{1}, 7:{4, 9}, 9:{6}, 6:{3, 8}, 3:{9}, 8:{2}, 2:{5}, 5:{8}}
-
-SCC(graph, 1)
-print finishing_time
-
-graph = {7:{1}, 4:{7}, 1:{4}, 9:{3, 7}, 6:{9}, 3:{6}, 8:{5, 6}, 5:{2}, 2:{8}}
-SCC(graph, 0)
-print leader
